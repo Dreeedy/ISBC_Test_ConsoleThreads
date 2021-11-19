c1d3206c2989bc54c43df3d1550eee17ba4cf92d
Dreeedy <dreeedyone@gmail.com>
2021-11-19 13:39:57 +0300

    Add project files.

---
+++ ISBC_Test_ConsoleThreads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISBC_Test_ConsoleThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
